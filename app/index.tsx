16ba14675390dc518141fc4a9e1cc12f313d0f64
hollali <dheztinykartel@gmail.com>
2025-07-05 03:34:59 +0000

    `Added navigation and cart functionality to the home screen`

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,36 @@
 import { images, offers } from "@/constants";
 import cn from 'clsx';
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function Index() {
 	return (
-		<SafeAreaView>
+		<SafeAreaView className="flex-1 bg-white">
+      <View className="flex-between flex-row w-full my-5 px-5">
+        <View className="flex-start">
+          <Text className="small-bold text-primary">DELIVER TO</Text>
+          <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+            <Text className="paragraph-bold text-dark-100">Accra</Text>
+            <Image source={images.arrowDown} className="size-3" resizeMode="contain" tintColor="#000000"/>
+          </TouchableOpacity>
+        </View>
+        <Text>Cart</Text>
+      </View>
       <FlatList data={offers}
       renderItem={({item,index}) =>{
         const isEven = index % 2 === 0;
         return (
           <View>
-            <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse':'flex-row')} style={{backgroundColor: item.color}}>
+            <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse':'flex-row')} style={{backgroundColor: item.color}}
+            android_ripple={{color: '#ffffff22'}}>
               {({pressed}) => (
                 <Fragment>
                   <View className={"h-full w-1/2"}>
                   <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
                   </View>
-                  <View className={"offer-card__info"}>
+                  <View className={cn("offer-card__info", isEven  ? 'pl-10' : 'pr-10')}>
                     <Text className={" h1-bold text-white loading-tight"}>{item.title}</Text>
                     <Image source={images.arrowRight} className="size-10" resizeMode="contain" tintColor="#ffffff"/>
                   </View>
@@ -28,7 +39,9 @@ export default function Index() {
             </Pressable>
           </View>
         )
-      }}/>
+      }}
+      contentContainerClassName="pb-28 px-5"
+      />
     </SafeAreaView>
 	);
 }
