af21a436373f3298b8b65017366fb06d5cc554bb
hollali <dheztinykartel@gmail.com>
2025-07-05 03:03:40 +0000

    Refactor offer card rendering and add clsx for conditional styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,30 @@
-import { FlatList, Pressable, View, Text } from "react-native";
+import { images, offers } from "@/constants";
+import cn from 'clsx';
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
-import { offers } from "@/constants";
 
 export default function Index() {
 	return (
 		<SafeAreaView>
       <FlatList data={offers}
       renderItem={({item,index}) =>{
+        const isEven = index % 2 === 0;
         return (
           <View>
-            <Pressable className="bg-amber-600 rounded-xl my-3 h-48">
-              <Text>{item.title}</Text>
+            <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse':'flex-row')} style={{backgroundColor: item.color}}>
+              {({pressed}) => (
+                <Fragment>
+                  <View className={"h-full w-1/2"}>
+                  <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
+                  </View>
+                  <View className={"offer-card__info"}>
+                    <Text className={" h1-bold text-white loading-tight"}>{item.title}</Text>
+                    <Image source={images.arrowRight} className="size-10" resizeMode="contain" tintColor="#ffffff"/>
+                  </View>
+                </Fragment>
+              )}
             </Pressable>
           </View>
         )
