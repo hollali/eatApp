b6b987cef088c652d1f4cdb5812451271f7ab619
hollali <dheztinykartel@gmail.com>
2025-07-05 09:47:45 +0000

    `Update layouts to use SafeAreaView and add authentication redirect`

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text,} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
 
 export default function _layout() {
 	return (
-		<View>
+		<SafeAreaView>
 			<Text>Auth_layout</Text>
-		</View>
+			<Slot />
+		</SafeAreaView>
 	);
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Slot } from "expo-router";
+import { Redirect, Slot } from "expo-router";
 
 export default function _Layout() {
+    const isAuthenticated = false; // Replace with actual authentication logic
+    if (!isAuthenticated) return <Redirect href="/sign-in" />;
 	return<Slot/>
 }
