f71c1756ac4e597c898990676f5bf3e2caf91fb8
hollali <dheztinykartel@gmail.com>
2025-07-07 08:14:30 +0000

    Add Appwrite configuration and custom components for authentication

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+    project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+	endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.hollali.eatApp",
+    databaseId: '686b7b4f000b31a8b2f6',
+    userCollectionId: '686b7b9f00160656de2a',
+};
+
