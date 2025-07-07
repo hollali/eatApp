a4e905e6936f349aea6d16669584a7adc5fdd506
hollali <dheztinykartel@gmail.com>
2025-07-07 10:37:46 +0000

    `Added margin to "Sign Up" link in sign-in page`

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -45,7 +45,7 @@ const SignIn = () => {
 				<View className="flex justify-center mt-5 flex-row gap-2">
 					<Text className="base-regular text-gray-100">
 						Don&#39;t have an account?
-						<Link href="/sign-up" className="base-bold text-primary">Sign Up</Link>
+						<Link href="/sign-up" className="base-bold text-primary ml-2">Sign Up</Link>
 					</Text>
 				</View>	
 		</View>
