b720f919423e78c66c0906f2b4c2deec7c1f5755
hollali <dheztinykartel@gmail.com>
2025-07-07 10:32:12 +0000

    Refactor SignIn component to include CustomInput and CustomButton, enhancing form functionality and user experience

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,53 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
-import { router } from "expo-router";
+import CustomButton from "@/components/customButton";
+import CustomInput from "@/components/customInput";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignIn = () => {
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [form, setForm] = useState({email: '', password: ''});
+	const submit = async () => {
+		if(!form.email || !form.password) Alert.alert('Error', 'Please Enter Valid Email Address & Password');{
+			setIsSubmitting(true);
+			try {
+				//! Appwrite Sign In Logic
+				Alert.alert('Success', 'You have successfully signed in!');
+				router.replace('/');
+			} catch (error : any) {
+				Alert.alert('Error', 'Failed to sign in. Please try again.');
+			} finally {
+				setIsSubmitting(false);
+			}
+		}
+	}
 	return (
-		<View>
-			<Text>SignIn</Text>
-            <Button title="Sign Up" onPress={() => router.push("/sign-up")} />
+		<View className="gap-10 bg-white rounded-lg p-5 mt-5">
+			<CustomInput
+				placeholder="Email"
+				value={form.email}
+				onChangeText={(text) => setForm({...form, email: text})}
+				label="Enter Email Address"
+				keyboardType="email-address"
+				/>
+				<CustomInput
+				placeholder="Password"
+				value={form.password}
+				onChangeText={(text) => setForm({...form, password: text})}
+				label="Enter Password"
+				secureTextEntry={true}
+				/>
+				<CustomButton
+				title="Sign In"
+				isLoading={isSubmitting}
+				onPress={submit}
+				/>
+				<View className="flex justify-center mt-5 flex-row gap-2">
+					<Text className="base-regular text-gray-100">
+						Don&#39;t have an account?
+						<Link href="/sign-up" className="base-bold text-primary">Sign Up</Link>
+					</Text>
+				</View>	
 		</View>
 	);
 };
