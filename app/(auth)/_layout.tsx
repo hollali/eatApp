6648237683466146d5b587d47dd2d5fb722d65a3
hollali <dheztinykartel@gmail.com>
2025-07-07 08:16:12 +0000

    Refactor layout component to import and use CustomButton and CustomInput

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,7 +1,9 @@
-import { View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image} from "react-native";
-import React from "react";
-import { Slot } from "expo-router";
+import CustomButton from "@/components/customButton";
+import CustomInput from "@/components/customInput";
 import { images } from "@/constants";
+import { Slot } from "expo-router";
+import React from "react";
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
 
 export default function _layout() {
 	return (
@@ -11,6 +13,8 @@ export default function _layout() {
 				<ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch"/>
 				<Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10"/>
 				</View>
+				<CustomInput/>
+				<CustomButton/>
 			</ScrollView>
 			<Slot />
 		</KeyboardAvoidingView>
