570bfc5f57d4459567707525e5d79ce170dbdcce
hollali <dheztinykartel@gmail.com>
2025-07-19 11:25:35 +0000

    `feat: Added expo-location and updated index.tsx to use it`

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,40 @@
+import CartButton from "@/components/cartButton";
 import { images, offers } from "@/constants";
+import useAuthStore from "@/store/auth.store";
 import cn from 'clsx';
-import { Fragment } from "react";
+import * as Location from "expo-location";
+import { Fragment, useEffect, useState } from "react";
 import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CartButton from "@/components/cartButton";
-import useAuthStore from "@/store/auth.store";
+
 
 export default function Index() {
   const {user} = useAuthStore();
+
+  const [city, setCity] = useState("Fetching...");
+
+  useEffect(() => {
+    (async () => {
+      // Request permissions
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setCity("Permission denied");
+        return;
+      }
+
+      // Get user's coordinates
+      let location = await Location.getCurrentPositionAsync({});
+      
+      // Reverse geocode to get city
+      let [place] = await Location.reverseGeocodeAsync(location.coords);
+
+      if (place?.city) {
+        setCity(place.city);
+      } else {
+        setCity("Unknown");
+      }
+    })();
+  }, []);
   
 	return (
 		<SafeAreaView className="flex-1 bg-white">
@@ -39,7 +66,7 @@ export default function Index() {
         <View className="flex-start">
           <Text className="small-bold text-primary">DELIVER TO</Text>
           <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
-            <Text className="paragraph-bold text-dark-100">Accra</Text>
+            <Text className="paragraph-bold text-dark-100">{city}</Text>
             <Image source={images.arrowDown} className="size-3" resizeMode="contain" tintColor="#000000"/>
           </TouchableOpacity>
         </View>
