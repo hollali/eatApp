694a347b06ce856f68cb0f0cb5baa9bbd631f8cd
hollali <dheztinykartel@gmail.com>
2025-07-20 09:26:51 +0000

    `Remove unused variable and condition in Search component`

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -26,7 +26,6 @@ const Search = () => {
                 data={data}
                 renderItem={({ item, index }) => {
                     const isFirstRightColItem = index % 2 === 0;
-
                     return (
                         <View className={cn("flex-1 max-w-[48%]", !isFirstRightColItem ? 'mt-10': 'mt-0')}>
                             <MenuCard item={item as MenuItem} />
